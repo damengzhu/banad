1aa9febc027b27d57f51c348e114725c00986e92
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-14 00:59:25 +0800

    fix

--- make.sh
+++ make.sh
@@ -5,6 +5,6 @@ echo -e "[Adblock Plus 2.0]\n! Title: 瞎弄的广告过滤规则\n! Homepage: h
 echo -e "#Title: 瞎弄的广告过滤规则\n#Homepage: https://github.com/damengzhu/banad\n#by: 酷安@大萌主\n#Total Count:$total\n#Update Time: $time\n127.0.0.1 localhost\n::1 localhost" > hosts.txt
 cat jiekouAD.txt|grep -E "^(\|\|)[^\/\^]+\^$" |sort |uniq>> dnslist.txt
 cat dnslist.txt |grep -Ev "!|\["|sed 's/||/0.0.0.0 /g' | sed "s/\^//g"|sort |uniq >> hosts.txt
-sed -i "s/! Update Time:.*/! Update Time: $time" jiekouAD.txt
+sed -i "s/! Update Time:.*/! Update Time: $time/g" jiekouAD.txt
 total=`cat jiekouAD.txt|grep -v "!"|wc -l`
-sed -i "s/! Total Count:.*/! Total Count: $total" jiekouAD.txt
+sed -i "s/! Total Count:.*/! Total Count: $total/g" jiekouAD.txt
