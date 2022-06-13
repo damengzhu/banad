da711e6b0eb7c1516f6056e08c9f2596e7e7e5e5
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-13 19:32:18 +0800

    add time

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+time=`echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " `
 total=`cat jiekouAD.txt|grep -E "^(\|\|)[^\/\^]+\^$"|wc -l`
-echo -e "[Adblock Plus 2.0]\n! Title: 瞎弄的广告过滤规则\n! Homepage: https://github.com/damengzhu/banad\n! by: 酷安@大萌主\n! Total Count:$total" >dnslist.txt
+echo -e "[Adblock Plus 2.0]\n! Title: 瞎弄的广告过滤规则\n! Homepage: https://github.com/damengzhu/banad\n! by: 酷安@大萌主\n! Total Count:$total\n! Update:$time" >dnslist.txt
 echo -e "#Title: 瞎弄的广告过滤规则\n#Homepage: https://github.com/damengzhu/banad\n#by: 酷安@大萌主\n#Total Count:$total\n127.0.0.1 localhost\n::1 localhost" > hosts.txt
 cat jiekouAD.txt|grep -E "^(\|\|)[^\/\^]+\^$" |sort |uniq>> dnslist.txt
 cat dnslist.txt |grep -Ev "!|\["|sed 's/||/0.0.0.0 /g' | sed "s/\^//g"|sort |uniq >> hosts.txt
